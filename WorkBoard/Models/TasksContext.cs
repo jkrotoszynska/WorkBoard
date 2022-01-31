5aff8056723a9112084e955fe5db8d8fffa51406
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2022-01-31 01:57:38 +0100

    naprawiam

--- WorkBoard/Models/TasksContext.cs
+++ WorkBoard/Models/TasksContext.cs
@@ -8,10 +8,7 @@ namespace WorkBoard.Models
 {
     public class TasksContext : DbContext
     {
-        public TasksContext(DbContextOptions<TasksContext> options) : base(options)
-        {
-
-        }
+        public TasksContext(DbContextOptions<TasksContext> options) : base(options){}
         public DbSet<Task> Countires { get; set; } // dokonaliśmy mapowania po kodzie (zamieniamy rekordy na obiekty)
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
