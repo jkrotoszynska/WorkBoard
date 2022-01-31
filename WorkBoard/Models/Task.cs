508169d2dac16e32f5d05d883f987d3ff5f569f6
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2022-01-31 01:23:33 +0100

    first step

---
+++ WorkBoard/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkBoard.Models
+{
+    public class Task
+    {
+        public int user_id { get; set; }
+        public int task_name{ get; set; }
+        public string status { get; set; }
+        public string description { get; set; }
+        public string team { get; set; }
+        public DateTime creation_date { get; set; }
+        public DateTime deadline_date { get; set; }
+        public DateTime modification_date { get; set; } 
+    }
+}
