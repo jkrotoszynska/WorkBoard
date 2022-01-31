c39476d751a5d1c952ee34bdb0216c11440d0a3c
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2022-01-31 01:47:48 +0100

    naprawiam

--- WorkBoard/Models/TasksContext.cs
+++ WorkBoard/Models/TasksContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WorkBoard.Models
 {
-    public class TasksContext
+    public class TasksContext : DbContext
     {
+        public TasksContext(DbContextOptions<TasksContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Task> Countires { get; set; } // dokonaliśmy mapowania po kodzie (zamieniamy rekordy na obiekty)
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().HasKey("task_id"); // wskazujemy klucz podstawowy encji
+            modelBuilder.Entity<Task>().ToTable("tasks"); 
+        }
+
     }
 }
