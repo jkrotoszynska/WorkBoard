ece55c48bc4b1925cfa4a56aa621d2e42c573e83
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2022-02-02 17:18:14 +0100

    login

--- WorkBoard/Controllers/AccountController.cs
+++ WorkBoard/Controllers/AccountController.cs
@@ -66,5 +66,29 @@ namespace WorkBoard.Controllers
 
             return View(model);
         }
+    
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            return View(model);
+        }
     }
 }
