985597481d9b3afde3b15d9e8ddbd6fa7d7533e4
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2022-02-02 20:00:03 +0100

    imie i nazwisko joł

--- WorkBoard/Data/ApplicationDbContext.cs
+++ WorkBoard/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WorkBoard.Models;
 
 namespace WorkBoard.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
