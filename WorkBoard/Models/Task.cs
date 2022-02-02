08a5ca60203a6e165b80b28f65726add13bcfd36
Kacper Jach <kacperini@gmail.com>
2022-02-02 16:32:21 +0100

    task order results

--- WorkBoard/Models/Task.cs
+++ WorkBoard/Models/Task.cs
@@ -8,36 +8,36 @@ namespace WorkBoard.Models
 {
     public class Task
     {
-        [Display(Name = "ID Tasku")]
-        [Required(ErrorMessage = "Podaj id tasku")]
+        [Display(Name = "Task ID")]
+        [Required(ErrorMessage = "Enter task ID")]
         // [IntLength(4, MinimumLength = 1, ErrorMessage = "Id tasku nie powinno przekraczać 4 cyfr")]
 
         public int task_id { get; set; }
 
-        [Display(Name = "ID Użytkownika")]
+        [Display(Name = "User")]
         public int user_id { get; set; }
 
-        [Display(Name = "Nazwa")]
+        [Display(Name = "Task name")]
         public string task_name{ get; set; }
 
         [Display(Name = "Status")]
         public string status { get; set; }
 
-        [Display(Name = "Opis")]
+        [Display(Name = "Desc")]
         public string description { get; set; }
 
-        [Display(Name = "Zespół")]
+        [Display(Name = "Team")]
         public string team { get; set; }
 
-        [Display(Name = "Data rozpoczęcia")]
+        [Display(Name = "Start Date")]
         [DisplayFormat(DataFormatString = "{0: dd-MM-yyyy}")]
         public DateTime creation_date { get; set; }
 
-        [Display(Name = "Data zakończenia")]
+        [Display(Name = "End Date")]
         [DisplayFormat(DataFormatString = "{0: dd-MM-yyyy}")]
         public DateTime deadline_date { get; set; }
 
-        [Display(Name = "Data modyfikacji")]
+        [Display(Name = "Mod. Date")]
         [DisplayFormat(DataFormatString = "{0: dd-MM-yyyy}")]
         public DateTime modification_date { get; set; } 
     }
